2795126dd67d0791d01c8ecbc3e907b272e5f40a
Tedis32 <teddrinkscoffee@gmail.com>
2021-02-06 12:04:28 +0000

    Some Chat UI Added
    TODO: Add functionality

--- lib/screens/tabs/chat.dart
+++ lib/screens/tabs/chat.dart
@@ -1,3 +1,7 @@
+import 'package:FYP_project/models/action_button.dart';
+import 'package:FYP_project/models/chat_message.dart';
+import 'package:FYP_project/models/message_input_box.dart';
+import 'package:FYP_project/models/page_wrapper.dart';
 import 'package:flutter/material.dart';
 
 class Chat extends StatefulWidget {
@@ -6,12 +10,43 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-    Container(
-      child: Center(child: Text("Insert chat functionality"))
-    ),);
+      body: FlatPageWrapper(
+        scrollType: ScrollType.floatingHeader,
+        reverseBodyList: true,
+        children: [
+          FlatChatMessage(
+            message: "",
+            messageType: MessageType.sent,
+            showTime: true,
+            time: "2 mins ago",
+          ),
+          
+          FlatChatMessage(
+            message: "",
+            showTime: true,
+            time: "2 mins ago",
+          ),
+          FlatChatMessage(
+            message: "",
+            showTime: true,
+            time: "2 mins ago",
+          ),
+        ],
+        footer: FlatMessageInputBox(
+          prefix: FlatActionButton(
+            iconData: Icons.add,
+            iconSize: 24.0,
+          ),
+          roundedCorners: true,
+          suffix: FlatActionButton(
+            iconData: Icons.image,
+            iconSize: 24.0,
+          ),
+        ),
+      ),
+    );
   }
 }
